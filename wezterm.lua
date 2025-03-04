0a03cb65ca46d7c82e124d100cdb4ba091ca28e3
eristocrates <eristocrates@gmail.com>
2025-03-03 23:40:23 -0500

    more or less finalized color selection

--- wezterm.lua
+++ wezterm.lua
@@ -215,24 +215,24 @@ config.color_schemes                = {
     -- Specifies the border color of the cursor when the cursor style is set to Block,
     -- or the color of the vertical or horizontal bar when the cursor style is set to
     -- Bar or Underline.
-    cursor_border = lead,
+    cursor_border = paris_paving,
 
     -- the foreground color of selected text
     selection_fg = plum,
     -- the background color of selected text
-    selection_bg = lead,
+    selection_bg = paris_paving,
 
     -- The color of the scrollbar "thumb"; the portion that represents the current viewport
-    scrollbar_thumb = '#222222',
+    scrollbar_thumb = lusty_lavendar,
 
     -- The color of the split lines between panes
-    split = '#444444',
+    split = tangled_web,
 
     ansi = {
       --[[
       base background highlight
       ]]
-      'black', -- terminal:black
+      lead, -- terminal:black
       --[[
       error text
       kak colon
@@ -309,26 +309,26 @@ config.color_schemes                = {
     -- When the IME, a dead key or a leader key are being processed and are effectively
     -- holding input pending the result of input composition, change the cursor
     -- to this color to give a visual cue about the compose state.
-    compose_cursor = 'orange',
+    compose_cursor = atomic_orange,
 
     -- Colors for copy_mode and quick_select
     -- available since: 20220807-113146-c2fee766
     -- In copy_mode, the color of the active text is:
     -- 1. copy_mode_active_highlight_* if additional text was selected using the mouse
     -- 2. selection_* otherwise
-    copy_mode_active_highlight_bg = { Color = '#000000' },
+    copy_mode_active_highlight_bg = { Color = death_by_chocolate },
     -- use `AnsiColor` to specify one of the ansi color palette values
     -- (index 0-15) using one of the names "Black", "Maroon", "Green",
     --  "Olive", "Navy", "Purple", "Teal", "Silver", "Grey", "Red", "Lime",
     -- "Yellow", "Blue", "Fuchsia", "Aqua" or "White".
-    copy_mode_active_highlight_fg = { AnsiColor = 'Black' },
-    copy_mode_inactive_highlight_bg = { Color = '#52ad70' },
-    copy_mode_inactive_highlight_fg = { AnsiColor = 'White' },
+    copy_mode_active_highlight_fg = { Color = dwarf_fortress },
+    copy_mode_inactive_highlight_bg = { Color = intergalactic },
+    copy_mode_inactive_highlight_fg = { Color = hint_of_red },
 
-    quick_select_label_bg = { Color = 'peru' },
-    quick_select_label_fg = { Color = '#ffffff' },
-    quick_select_match_bg = { AnsiColor = 'Navy' },
-    quick_select_match_fg = { Color = '#ffffff' },
+    quick_select_label_bg = { Color = cacodemon_red },
+    quick_select_label_fg = { Color = doctor },
+    quick_select_match_bg = { Color = dragon_ball },
+    quick_select_match_fg = { Color = hint_of_red },
   },
   ['ow-the-edge-old'] = {
     foreground = inca_yellow,      -- terminal:foreground --editor text
